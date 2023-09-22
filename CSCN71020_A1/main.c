f35b9d9bd9103fbc230e0987450e80cea8ff250d
Tao-Z-Boyce <Tboyce8355@conestogac.on.ca>
2023-09-22 09:56:22 -0400

    Fixing subtract function

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,9 +3,10 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
-void main() {
+void main(void) {
 
 	printWelcomeMenu();
 
@@ -20,23 +21,25 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
 
 }
 
-void printWelcomeMenu() {
+void printWelcomeMenu(void) {
 	printf(" **********************\n");
 	printf("**   Welcome to the   **\n");
 	printf("**   BCS Calculator   **\n");
 	printf(" **********************\n");
 }
 
-void printOptions() {
+void printOptions(void) {
 	printf("1. Add\n");
 	printf("2. Subtract\n");
 }
 
-void add() {
+void add(void) {
 	double num1, num2, result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
@@ -44,4 +47,14 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract(void) {
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
